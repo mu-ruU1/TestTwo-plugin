54eca85156f43a0c62c53b2aa81cf0897812e8d2
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-02-16 17:21:54 +0900

    環境構築だけ

---
+++ src/main/java/com/yuu/testtwo/TestTwo.java
@@ -0,0 +1,17 @@
+package com.yuu.testtwo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestTwo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
